0e2e3dd4541c55a6922a9cd760dc1a7677a23fb7
huan-yp <3051561876@qq.com>
2025-04-29 00:00:05 +0800

    feat: 铁甲牛仔活动

--- event.py
+++ event.py
@@ -1,18 +1,12 @@
-from autowsgr.fight.event.event_2025_0123 import EventFightPlan20250123
+from autowsgr.fight.event.event_2025_0424 import EventFightPlan2025_0424
 from autowsgr.scripts.main import start_script
 
+timer = start_script()
 
-timer = start_script('./user_settings.yaml')
-# set_support(timer,True) # 如果要在战斗前开启战役支援请取消这一行的注释
-plan = EventFightPlan20250123(
-    timer,
-    plan_path='H1AB炸鱼',
-    fleet_id=4,
-)
-# plan_path处可填写plan的yaml文件的绝对地址, 也可使用相对地址直接填写plan的文件名
-# 详细的plan名可在data/plans/event/20250123查看，fleet_id为出击编队
+# E1 炸鱼, 替换成你自己电脑上的绝对路径
+e1_plan = EventFightPlan2025_0424(timer, r'C:\Users\huany\Desktop\Projects\Auto-WSGR-dev\AutoWSGR\examples\plans\event\20250424\E1炸鱼.yaml')
+e1_plan.run_for_times(200)
 
-
-plan.run_for_times(
-    500,
-)  # 第一个参数是战斗次数,还有个可选参数为检查远征时间，默认为1800S
+# E2 战术
+# e2_plan = EventFightPlan2025_0424(timer, r'C:\Users\huany\Desktop\Projects\Auto-WSGR-dev\AutoWSGR\examples\plans\event\20250424\E2战术.yaml') 
+# e2_plan.run_for_times(10)
