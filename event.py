c8e164c2c8f39b6a88c6f03b2e17ca4a76bc8089
YaoerWu <honey_yolk@outlook.com>
2025-09-30 16:42:02 +0800

    feat: 20250930勇往直前周年活动

--- event.py
+++ event.py
@@ -1,14 +1,14 @@
-from autowsgr.fight.event.event_2025_0710 import EventFightPlan20250710
+from autowsgr.fight.event.event_2025_0930 import EventFightPlan20250930
 from autowsgr.scripts.main import start_script
 
 
 timer = start_script("./user_settings.yaml")
 # set_support(timer,True) # 如果要在战斗前开启战役支援请取消这一行的注释
-plan = EventFightPlan20250710(
+plan = EventFightPlan20250930(
     timer,
-    plan_path="E9AC夜战",
+    plan_path="E11ACD夜战",
     fleet_id=2,
-)  # 修改E9AC夜战为相对于的plan，详细的plan名可在data/plans/event/20250710查看，fleet_id为出击编队
+)  # 修改E11ACD夜战为相对于的plan，详细的plan名可在data/plans/event/20250930查看，fleet_id为出击编队
 
 
 plan.run_for_times(
